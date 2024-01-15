0bca3f0f44c9844013621a3695de42dff9ca48d6
AKDA2318 <89972326+AKDA2318@users.noreply.github.com>
2024-01-15 20:41:53 +0100

    Add project files.Add files via upload

---
+++ Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decoder {
+    internal class Data {
+        public Data(List<int> order, List<string> words) {
+            Order = order;
+            Words = words;
+        }
+
+        public List<int> Order { get; set; }
+        public List<string> Words { get; set; }
+    }
+}
